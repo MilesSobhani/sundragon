8cfe8dd898fdcef31e87e18a7dea6daa7f294465
MilesSobhani <38140009+MilesSobhani@users.noreply.github.com>
2020-02-12 23:09:27 -0600

    just getting started

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        THIS WILL BE JESS's WEBSITE AT SOME TIME IN THE FUTURE!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
